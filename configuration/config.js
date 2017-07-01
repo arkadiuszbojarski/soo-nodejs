5caa2ea105c8fa652f35a682d14f6c25b93ccba1
Arkadiusz Bojarski <Arkadiusz Bojarski>
2017-07-01 08:36:53 +0200

    Configured application to use local default mongodb instance.

--- configuration/config.js
+++ configuration/config.js
@@ -2,19 +2,15 @@
 module.exports = {
     app: {
         // Server IP
-        ip: process.env.OPENSHIFT_NODEJS_IP
-                || process.env.ip
-                || undefined,
+        ip: process.env.ip || undefined,
 
         // Server port
-        port: process.env.OPENSHIFT_NODEJS_PORT
-                || process.env.PORT
-                || 3000
+        port: process.env.PORT || 3000
     },
 
     // MongoDB connection options
     mongo: {
-        uri: process.env.MONGODB_URI || 'mongodb://sooserver:arbulon@ds060749.mlab.com:60749/soodb'
+        uri: process.env.MONGODB_URI || 'mongodb://localhost/soo'
     },
     jwt: {
         secret: 'secret',
